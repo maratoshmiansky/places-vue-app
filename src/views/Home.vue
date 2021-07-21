076afe5798cdd54cd946bb0f5844442d5eed5d01
Marat Oshmiansky <maratical@gmail.com>
2021-07-21 08:17:29 -0700

    added CRUD action methods

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,86 @@
 <template>
   <div class="home">
     <h1>{{ message }}</h1>
+    <div>
+      Name:
+      <input type="text" v-model="newPlaceParams.name" />
+      Address:
+      <input type="text" v-model="newPlaceParams.address" />
+    </div>
+    <button v-on:click="createPlace()">Create a new place!</button>
+    <div v-for="place in places" v-bind:key="place.id">
+      <h2>Name: {{ place.name }}</h2>
+      <p>Address: {{ place.address }}</p>
+      <button v-on:click="showPlace(place)">More info!</button>
+    </div>
+    <dialog id="place-details">
+      <form method="dialog">
+        <h1>Place Info!</h1>
+        <p>
+          Name:
+          <input type="text" v-model="currentPlace.name" />
+        </p>
+        <p>
+          Address:
+          <input type="text" v-model="currentPlace.address" />
+        </p>
+        <button v-on:click="updatePlace(currentPlace)">Update</button>
+        <button v-on:click="destroyPlace(currentPlace)">Destroy</button>
+        <button>Close</button>
+      </form>
+    </dialog>
   </div>
 </template>
-
 <style></style>
-
 <script>
+import axios from "axios";
 export default {
   data: function () {
     return {
-      message: "Welcome to Vue.js!",
+      message: "Welcome to places-vue-app!",
+      places: [],
+      newPlaceParams: {},
+      currentPlace: {},
     };
   },
-  created: function () {},
-  methods: {},
+  created: function () {
+    this.indexPlaces();
+  },
+  methods: {
+    indexPlaces: function () {
+      axios.get("http://localhost:3000/places").then((response) => {
+        this.places = response.data;
+        console.log("All places:", this.places);
+      });
+    },
+    createPlace: function () {
+      console.log("Creating place");
+      axios
+        .post("http://localhost:3000/places", this.newPlaceParams)
+        .then((response) => {
+          console.log("Success!", response.data);
+          this.places.push(response.data);
+        })
+        .catch((error) => console.log(error.response));
+    },
+    showPlace: function (place) {
+      console.log(place);
+      this.currentPlace = place;
+      document.querySelector("#place-details").showModal();
+    },
+    updatePlace: function (place) {
+      var editPlaceParams = place;
+      axios.patch("http://localhost:3000/places/" + place.id, editPlaceParams).then((response) => {
+        console.log("Success!", response.data);
+      });
+    },
+    destroyPlace: function (place) {
+      axios.delete("http://localhost:3000/places/" + place.id).then((response) => {
+        console.log("Success!", response.data);
+        var index = this.places.indexOf(place);
+        this.places.splice(index, 1);
+      });
+    },
+  },
 };
 </script>
